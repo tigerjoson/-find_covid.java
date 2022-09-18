ba2ea014bfecf98244050101f9b8ef900e588d4f
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2022-09-18 21:15:44 +0800

    Update Printinfo_ods.java

--- Printinfo_ods.java
+++ Printinfo_ods.java
@@ -7,7 +7,7 @@ import org.odftoolkit.simple.SpreadsheetDocument;
 import org.odftoolkit.simple.table.Cell;
 import org.odftoolkit.simple.table.Table;
 
-public class Printinfo_ods {
+public class access_ods {
 	private File ods_file;
 
 	public File getods_file() {
@@ -18,17 +18,18 @@ public class Printinfo_ods {
 		this.ods_file = ods_file;
 	}
 
-	public Printinfo_ods()  {}
-		
+	public access_ods() {
+	}
+
 	public void print_base_info() throws Exception {
 		SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.loadDocument(getods_file());
 		Table activetable = spreadsheetDocument.getSheetByIndex(0);
-		System.out.println("spreadsheetDocument.getTableList().size()="+spreadsheetDocument.getTableList().size());
-		System.out.println("activetable_name= "+activetable.getTableName());
+		System.out.println("spreadsheetDocument.getTableList().size()=" + spreadsheetDocument.getTableList().size());
+		System.out.println("activetable_name= " + activetable.getTableName());
 		System.out.println("activetable.getHeaderColumnCount()=" + activetable.getHeaderColumnCount());
 		System.out.println("activetable.getColumnCount()=" + activetable.getColumnCount());
 		System.out.println("activetable.getRowCount()=" + activetable.getRowCount());
-		
+
 	}
 
 	public void print_all_cell(int Max_value_want_to_see) throws Exception {
@@ -54,9 +55,9 @@ public class Printinfo_ods {
 		}
 	}
 
-	public  void print_all_cell() throws Exception {
+	public void print_all_cell() throws Exception {
 		if (getods_file().exists()) {
-			
+
 			SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.loadDocument(getods_file());
 			// beging from zero
 			Table activetable = spreadsheetDocument.getSheetByIndex(0);
@@ -74,15 +75,15 @@ public class Printinfo_ods {
 			System.out.println("ods_file.exists()=" + getods_file().exists());
 		}
 	}
-	
+
 	public ArrayList<Cell> all_data(int index_of_shhet) throws Exception {
 		File file = getods_file();
 		SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.loadDocument(file);
 		Table activetable = spreadsheetDocument.getSheetByIndex(index_of_shhet);
-		ArrayList<Cell> arrayList= new ArrayList<Cell>() ;
+		ArrayList<Cell> arrayList = new ArrayList<Cell>();
 		for (int i = 0, j = 0; i < activetable.getColumnCount() || j < activetable.getRowCount(); i++, j++) {
 			Cell cell = activetable.getCellByPosition(i, j);
-			//System.out.println("cell.getDisplayText()=" + cell.getDisplayText());
+			// System.out.println("cell.getDisplayText()=" + cell.getDisplayText());
 			arrayList.add(cell);
 		}
 		return arrayList;
